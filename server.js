a8b05467bf2fda76bf67bbd863752bc47acb7674
Bruno Henry <brunohenryt@hotmail.com>
2018-07-26 23:49:18 -0300

    proxy server api only

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-const express = require('express')
-const app = express()
-const FormData = require('form-data');
+const proxy = require('express-http-proxy');
+const app = require('express')();
 
-const imagerecognition = require('./services/imageRecognition')
-
-app.post('/imagerecognition', (req, res) => {
-    console.log(req)
-    imagerecognition()
-        .then(res =>  res.json({message: 'then'}))
-        .catch(() => res.json({ message: 'catch' }))
-})
-
-
+app.use('/proxy', proxy('gateway.watsonplatform.net', {
+    https: true
+  }));
 
 const port = 3001;
 app.listen(port, () => {
